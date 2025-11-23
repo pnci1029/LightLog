e945c1ed1f895cc4b1c50d0f1473c3bc1eac2ddf
chhong <pnci1029@gmail.com>
2025-11-23 19:58:49 +0900

    백엔드 소스 수정중

--- LightLog_BE/build.gradle.kts
+++ LightLog_BE/build.gradle.kts
@@ -29,15 +29,9 @@ dependencies {
     developmentOnly("org.springframework.boot:spring-boot-devtools") // Optional: for hot-reloading
 
     // JWT Libraries
-    implementation("io.jsonwebtoken:jjwt-api:0.12.5") {
-        isForce = true
-    }
-    implementation("io.jsonwebtoken:jjwt-impl:0.12.5") {
-        isForce = true
-    }
-    implementation("io.jsonwebtoken:jjwt-jackson:0.12.5") {
-        isForce = true
-    }
+    implementation("io.jsonwebtoken:jjwt-api:0.12.5")
+    implementation("io.jsonwebtoken:jjwt-impl:0.12.5")
+    implementation("io.jsonwebtoken:jjwt-jackson:0.12.5")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
